c59d2cc55112fd1c99d578aa9efa97600442e48a
Victor Raul <v1c70rotakmer@gmail.com>
2023-08-15 20:01:57 -0500

    Agregar responsive a todas las secciones

--- js/app.js
+++ js/app.js
@@ -10,7 +10,16 @@ document.addEventListener("scroll",()=>{
     }
 })
 
-
+const navbar = document.querySelector(".nav_links");
+const toggleMenu = document.querySelector(".btn_menu");
+toggleMenu.addEventListener("click",()=>{
+    toggleMenu.classList.toggle("active");
+    navbar.classList.toggle("active");
+})
+window.addEventListener("scroll",()=>{
+    navbar.classList.remove("active");
+    toggleMenu.classList.remove("active");
+})
 
 const heroContent = document.querySelector("#hero-imgs");
 
@@ -27,14 +36,28 @@ function createImgHero(){
 
 const navLinks = document.querySelectorAll(".nav_link");
 let navLinkAfter = 0;
-navLinks.forEach((navLink,i)=>{
-    navLink.addEventListener("click",()=>{
-        navLinks[navLinkAfter].classList.remove("active");
-        navLink.classList.add("active");
-        navLinkAfter = i;
+// navLinks.forEach((navLink,i)=>{
+//     navLink.addEventListener("click",()=>{
+//         navLinks[navLinkAfter].classList.remove("active");
+//         navLink.classList.add("active");
+//         navLinkAfter = i;
+//     })
+// })
+
+const sections = document.querySelectorAll("section");
+sections.forEach((sec,i)=>{
+    document.addEventListener("scroll",()=>{
+        const rect = sec.getBoundingClientRect();
+        if (rect.top <= 0 && rect.bottom >= 0) {
+            // La sección está visible en la ventana
+            navLinks[navLinkAfter].classList.remove("active");
+            navLinks[i].classList.add("active");
+            navLinkAfter=i;
+            // Añade la clase activa correspondiente
+            console.log
+        }
     })
 })
-
 
 
 createImgHero();
